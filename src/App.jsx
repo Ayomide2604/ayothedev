e9c09ac18704b7bde1c2f3b3b78ca198da148f60
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-28 20:33:39 +0100

    Add Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Hero from "./pages/Hero";
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Hero />
 		</>
 	);
 }
